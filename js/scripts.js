0b9b6306a91eb185220caf53131d2d65c2d36e34
Mohammed Choudhury <mosminecraft4@gmail.com>
2022-04-09 20:34:45 +0100

    g

--- js/scripts.js
+++ js/scripts.js
@@ -878,3 +878,13 @@ $('[data-carousel="swiper"]').each(function () {
         var init = new Swiper(initID, conf);
     };
 });
+
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-body .ptext').html(recipient)
+  })
